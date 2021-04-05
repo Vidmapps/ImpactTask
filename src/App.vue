1416d911948a1ebc152b00993a38d508338a839f
Vidmasas <vidmantas.burba@shopify.com>
2021-04-05 15:55:24 +0300

    Add main font

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,14 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    font-size: 20px;
+    color: #6e6e6e;
+  }
+
   .flex-and-center {
     display: -webkit-box;
     display: -ms-flexbox;
@@ -36,6 +44,7 @@ export default {
     -ms-flex-align: center;
     align-items: center;
   }
+
   .flex-and-space-between {
     display: -webkit-box;
     display: -ms-flexbox;
@@ -44,10 +53,13 @@ export default {
     -ms-flex-pack: justify;
     justify-content: space-between;
   }
+
   .pointer {
     cursor: pointer;
   }
+
   .uppercasing {
     text-transform: uppercase;
   }
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,9 @@
   <div>
     <header class="flex-and-space-between">
       <div class="header flex-and-center">
-        <img src="https://screenshot.click/05-45-0ikmf-tru0s.png">
+        <img src="https://screenshot.click/05-45-0ikmf-tru0s.png" class="header-logo">
       </div>
-      <nav class="flex-and-center uppercasing">
+      <nav class="flex-and-center uppercasing navigation">
         <div class="header-nav-item pointer">Home</div>
         <div class="header-nav-item pointer">Shop</div>
         <div class="header-nav-item pointer">Reviews</div>
@@ -26,7 +26,19 @@ export default {
     height: 56px;
     margin: 10px 10%;
   }
+
+  @media (max-width: 640px) {
+    .header {
+      margin: 0;
+    }
+  }
+
+  .header-logo {
+    width: 80px;
+  }
+
   .header-nav-item {
     margin-right: 32px;
   }
+
 </style>
