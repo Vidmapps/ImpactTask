818c570e79b432434630967f9a30a3533f9d688b
Vidmasas <vidmantas.burba@shopify.com>
2021-04-05 15:22:05 +0300

    Add header content

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <navigation></navigation>
-    <slider></slider>
-    <grid-list></grid-list>
+    <Header></Header>
+    <Slider></Slider>
+    <Grid-list></Grid-list>
     <FAQs></FAQs>
     <Footer></Footer>
   </div>
@@ -10,7 +10,7 @@
 
 <script>
 
-import Navigation from "./components/Navigation";
+import Header from "./components/Header";
 import Slider from "./components/Slider";
 import GridList from "./components/GridList";
 import FAQs from "./components/FAQs";
@@ -22,11 +22,32 @@ export default {
     FAQs,
     GridList,
     Slider,
-    Navigation
+    Header
   }
 }
 </script>
 
 <style>
-
+  .flex-and-center {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+  }
+  .flex-and-space-between {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: justify;
+    -ms-flex-pack: justify;
+    justify-content: space-between;
+  }
+  .pointer {
+    cursor: pointer;
+  }
+  .uppercasing {
+    text-transform: uppercase;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <header class="flex-and-space-between">
+      <div class="header flex-and-center">
+        <img src="https://screenshot.click/05-45-0ikmf-tru0s.png">
+      </div>
+      <nav class="flex-and-center uppercasing">
+        <div class="header-nav-item pointer">Home</div>
+        <div class="header-nav-item pointer">Shop</div>
+        <div class="header-nav-item pointer">Reviews</div>
+        <div class="header-nav-item pointer">Blog</div>
+      </nav>
+      <div></div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style>
+  .header {
+    height: 56px;
+    margin: 10px 10%;
+  }
+  .header-nav-item {
+    margin-right: 32px;
+  }
+</style>
