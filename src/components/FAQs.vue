a2b5ee7661d2462e6a32dc12b3d8b3c3673caa18
Vidmasas <vidmantas.burba@shopify.com>
2021-04-07 17:51:37 +0300

    Add FAQ content

--- src/components/FAQs.vue
+++ src/components/FAQs.vue
@@ -1,15 +1,157 @@
 <template>
- <div>
+ <div class="content-margin">
   <h2 class="center">FREQUENTLY ASKED QUESTIONS (FAQs)</h2>
- </div>
+   <div class="content-margin">
+     <div class="faq-container">
+       <div class="faq-dropdown flex-and-space-between">
+         <div class="faq-question uppercasing">Lorem Ipsum has been the industry's standard</div>
+         <div class="faq-toggle" @click="showElement('a1', 'b1', 'c1')">
+           <img style="display: block" id="c1" src="https://screenshot.click/07-39-iqdb4-ynk2b.png">
+           <img style="display: none" id="b1" src="https://screenshot.click/07-40-ptzk5-ostec.png">
+         </div>
+       </div>
+       <hr>
+       <div class="faq-answer" style="display: none" id="a1">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
+     </div>
+     <div>
+       <div class="faq-dropdown flex-and-space-between">
+         <div class="faq-question uppercasing">Lorem Ipsum has been the industry's standard</div>
+         <div class="faq-toggle" @click="showElement('a2', 'b2', 'c2')">
+           <img style="display: block" id="c2" src="https://screenshot.click/07-39-iqdb4-ynk2b.png">
+           <img style="display: none" id="b2" src="https://screenshot.click/07-40-ptzk5-ostec.png">
+         </div>
+       </div>
+       <hr>
+       <div class="faq-answer" style="display: none" id="a2">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
+     </div>
+     <div>
+       <div class="faq-dropdown flex-and-space-between">
+         <div class="faq-question uppercasing">Lorem Ipsum has been the industry's standard</div>
+         <div class="faq-toggle" @click="showElement('a3', 'b3', 'c3')">
+           <img style="display: block" id="c3" src="https://screenshot.click/07-39-iqdb4-ynk2b.png">
+           <img style="display: none" id="b3" src="https://screenshot.click/07-40-ptzk5-ostec.png">
+         </div>
+       </div>
+       <hr>
+       <div class="faq-answer" style="display: none" id="a3">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
+     </div>
+     <div>
+       <div class="faq-dropdown flex-and-space-between">
+         <div class="faq-question uppercasing">Lorem Ipsum has been the industry's standard</div>
+         <div class="faq-toggle" @click="showElement('a4', 'b4', 'c4')">
+           <img style="display: block" id="c4" src="https://screenshot.click/07-39-iqdb4-ynk2b.png">
+           <img style="display: none" id="b4" src="https://screenshot.click/07-40-ptzk5-ostec.png">
+         </div>
+       </div>
+       <hr>
+       <div class="faq-answer" style="display: none" id="a4">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
+     </div>
+     <div>
+       <div class="faq-dropdown flex-and-space-between">
+         <div class="faq-question uppercasing">Lorem Ipsum has been the industry's standard</div>
+         <div class="faq-toggle" @click="showElement('a5', 'b5', 'c5')">
+           <img style="display: block" id="c5" src="https://screenshot.click/07-39-iqdb4-ynk2b.png">
+           <img style="display: none" id="b5" src="https://screenshot.click/07-40-ptzk5-ostec.png">
+         </div>
+       </div>
+       <hr>
+       <div class="faq-answer" style="display: none" id="a5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
+     </div>
+     <div>
+       <div class="faq-dropdown flex-and-space-between">
+         <div class="faq-question uppercasing">Lorem Ipsum has been the industry's standard</div>
+         <div class="faq-toggle" @click="showElement('a6', 'b6', 'c6')">
+           <img style="display: block" id="c6" src="https://screenshot.click/07-39-iqdb4-ynk2b.png">
+           <img style="display: none" id="b6" src="https://screenshot.click/07-40-ptzk5-ostec.png">
+         </div>
+       </div>
+       <hr>
+       <div class="faq-answer" style="display: none" id="a6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
+     </div>
+     <div>
+       <div class="faq-dropdown flex-and-space-between">
+         <div class="faq-question uppercasing">Lorem Ipsum has been the industry's standard</div>
+         <div class="faq-toggle" @click="showElement('a7', 'b7', 'c7')">
+           <img style="display: block" id="c7" src="https://screenshot.click/07-39-iqdb4-ynk2b.png">
+           <img style="display: none" id="b7" src="https://screenshot.click/07-40-ptzk5-ostec.png">
+         </div>
+       </div>
+       <hr>
+       <div class="faq-answer" style="display: none" id="a7">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
+     </div>
+     <div>
+       <div class="faq-dropdown flex-and-space-between">
+         <div class="faq-question uppercasing">Lorem Ipsum has been the industry's standard</div>
+         <div class="faq-toggle" @click="showElement('a8', 'b8', 'c8')">
+           <img style="display: block" id="c8" src="https://screenshot.click/07-39-iqdb4-ynk2b.png">
+           <img style="display: none" id="b8" src="https://screenshot.click/07-40-ptzk5-ostec.png">
+         </div>
+       </div>
+       <hr>
+       <div class="faq-answer" style="display: none" id="a8">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
+     </div>
+     <div>
+       <div class="faq-dropdown flex-and-space-between">
+         <div class="faq-question uppercasing">Lorem Ipsum has been the industry's standard</div>
+         <div class="faq-toggle" @click="showElement('a9', 'b9', 'c9')">
+           <img style="display: block" id="c9" src="https://screenshot.click/07-39-iqdb4-ynk2b.png">
+           <img style="display: none" id="b9" src="https://screenshot.click/07-40-ptzk5-ostec.png">
+         </div>
+       </div>
+       <hr>
+       <div class="faq-answer" style="display: none" id="a9">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
+     </div>
+   </div>
+  </div>
 </template>
 
 <script>
+
+function toggleElement(id)
+{
+  if(document.getElementById(id).style.display == 'none')
+  {
+    document.getElementById(id).style.display = '';
+  }
+  else
+  {
+    document.getElementById(id).style.display = 'none';
+  }
+}
+
 export default {
-  name: "FAQs"
+  name: "FAQs",
+  data() {
+    return {
+      isShowAnswer: false
+    }
+  },
+  methods: {
+    showElement(a, b, c) {
+      toggleElement(a)
+      toggleElement(b)
+      toggleElement(c)
+    }
+  }
 }
 </script>
 
 <style scoped>
+
+h2 {
+  margin: 60px 0 30px;
+}
+
+.faq-container {
+  margin: 15px 0;
+}
+
+.faq-dropdown {
+  margin: 15px 0;
+}
+
+.faq-answer {
+  margin: 15px 0 25px;
+}
 
 </style>
